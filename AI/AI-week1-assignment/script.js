4e22cea45f5a90c1219e5a420690a953f3461fb6
Iris Bakran <bakraniris@gmail.com>
2025-10-28 14:12:36 +0100

    edinting js with buttong change back to pink color

--- AI/AI-week1-assignment/script.js
+++ AI/AI-week1-assignment/script.js
@@ -1,5 +1,12 @@
-const button = document.querySelector(".color-button");
+const button = document.querySelector('.color-button');
+let isDark = false;
 
-button.addEventListener("click", () => {
-  document.body.style.backgroundColor = "#5b2a86";
+button.addEventListener('click', () => {
+  if (!isDark) {
+    document.body.style.backgroundColor = '#5b2a86';
+    isDark = true;
+  } else {
+    document.body.style.backgroundColor = '#ffd3e6';
+    isDark = false;
+  }
 });
