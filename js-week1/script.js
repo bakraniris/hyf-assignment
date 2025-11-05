a9230269e580f28644faf810ee1d9cfecc498c05
Iris Bakran <bakraniris@gmail.com>
2025-11-05 13:57:26 +0100

    adding Goodboy-Oldboy (A dog age calculator) in js

--- js-week1/script.js
+++ js-week1/script.js
@@ -6,3 +6,14 @@ let age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in the year " + yearFuture + ".");  
 
 
+let dogYearOfBirth = 2015;
+let dogYearFuture = 2030;
+let dogYear = (dogYearFuture - dogYearOfBirth)*7;
+
+let shouldShowResultInDogYears = false;
+
+if (shouldShowResultInDogYears === true) {
+    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture + ".");
+}   else {
+    console.log("Your dog will be " + (dogYearFuture - dogYearOfBirth) + " human years old in " + dogYearFuture + ".");
+}
