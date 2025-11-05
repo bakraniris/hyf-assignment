a2e523a11c666e8c21f8ca4bd8885a5dac5654c8
Iris Bakran <bakraniris@gmail.com>
2025-11-05 14:23:34 +0100

    adding clarifications in console log for the house price estimator

--- js-week1/script.js
+++ js-week1/script.js
@@ -1,3 +1,4 @@
+// Age calculator
 let yearOfBirth = 1998;
 let yearFuture = 2030;
 
@@ -5,7 +6,7 @@ let age = yearFuture - yearOfBirth;
 
 console.log("You will be " + age + " years old in the year " + yearFuture + ".");  
 
-
+// Dog age calculator
 let dogYearOfBirth = 2015;
 let dogYearFuture = 2030;
 let dogYear = (dogYearFuture - dogYearOfBirth)*7;
@@ -17,3 +18,24 @@ if (shouldShowResultInDogYears === true) {
 }   else {
     console.log("Your dog will be " + (dogYearFuture - dogYearOfBirth) + " human years old in " + dogYearFuture + ".");
 }
+
+// House price estimator 
+let houseWidth = 8;
+let houseDepth = 10;
+let houseHeight = 10;
+let gardenSize = 100;
+let volumeInMeters = houseWidth * houseDepth * houseHeight; 
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSize * 300;
+
+console.log("The house price should be: " + housePrice + " DKK.");
+
+let peterHousePrice = 2500000;
+
+if (peterHousePrice > housePrice) {
+    console.log("Peter is paying too much, because the house price should be: " + housePrice + " DKK.");
+}   else if (peterHousePrice < housePrice) {
+    console.log("Peter is paying too little, because the house price should be: " + housePrice + " DKK.");
+}   else {
+    console.log("Peter is paying the correct price.");
+}
+
