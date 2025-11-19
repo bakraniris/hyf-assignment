dd43901d1a8f30d1edd6e3029bab7e1bf1619527
Iris Bakran <bakraniris@gmail.com>
2025-11-19 12:25:28 +0100

    adding a second task

--- js-week3/index.js
+++ js-week3/index.js
@@ -20,3 +20,24 @@ if (nameToRemoveIndex !== -1) {
 console.log(names);
 
 // Im going to try and add another solution that can eliminate "ahmad", not only "Ahmad"
+
+// Javascript warmup - When will we be there??
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function calculatingTravelTime(travelInformation) {
+  const hours = travelInformation.destinationDistance / travelInformation.speed;
+  const totalMinutes = hours * 60;
+
+  return {
+    hours: Math.floor(hours),
+    minutes: Math.round(totalMinutes % 60),
+  };
+}
+
+const travelTime = calculatingTravelTime(travelInformation);
+
+console.log(travelTime); // 8 hours and 38 minutes
