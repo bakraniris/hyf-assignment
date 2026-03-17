089b63693682da348394a626bf2f2d0a9e20b573
Iris Bakran <bakraniris@gmail.com>
2026-03-17 14:04:17 +0100

    adding a button, and fixing previous function to work with the button

--- advanced-js-week2/script.js
+++ advanced-js-week2/script.js
@@ -4,13 +4,16 @@ setTimeout(() => {
   document.getElementById("text1").innerText = "This is the text displayed after 2.5 seconds";
 }, 2500);
 
-function logAfterDelay(delay, stringToLog) {
+function logAfterDelay(delay, stringToLog, elementId) {
   setTimeout(() => {
-    document.getElementById("text2").innerText = stringToLog;
+    document.getElementById(elementId).innerText = stringToLog;
   }, delay * 1000);
 }
 
-logAfterDelay(3, "Logged after 3 seconds");
-logAfterDelay(5, "Logged after 5 seconds");
-logAfterDelay(10, "Logged after 10 seconds");
+logAfterDelay(3, "Logged after 3 seconds", "text2");
+logAfterDelay(5, "Logged after 5 seconds", "text2");
+logAfterDelay(10, "Logged after 10 seconds", "text2");
 
+document.getElementById("button").addEventListener("click", () => {
+  logAfterDelay(5, "Called after 5 seconds", "text3");
+});
